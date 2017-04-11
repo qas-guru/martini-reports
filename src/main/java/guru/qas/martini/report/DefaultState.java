6e7a8200eb9f32b8b4268d18a6ac99e749f0ca85
pcurich <pcurich@digitalmeasures.com>
2017-04-11 14:38:46 -0500

    Changed font for compromised themes.

--- src/main/java/guru/qas/martini/report/DefaultState.java
+++ src/main/java/guru/qas/martini/report/DefaultState.java
@@ -18,6 +18,7 @@ package guru.qas.martini.report;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -32,7 +33,10 @@ import org.apache.poi.ss.usermodel.BorderStyle;
 import org.apache.poi.ss.usermodel.FillPatternType;
 import org.apache.poi.ss.usermodel.IndexedColors;
 
+import com.google.common.base.Splitter;
 import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.Sets;
 import com.google.gson.JsonObject;
@@ -83,6 +87,7 @@ public class DefaultState implements State {
 	public void updateResults() {
 		updateLongestExecutions();
 		colorRowsByStatus();
+		colorCompromisedThemes();
 	}
 
 	public void updateLongestExecutions() {
@@ -200,6 +205,84 @@ public class DefaultState implements State {
 			}
 		}
 	}
+
+	protected void colorCompromisedThemes() {
+		Collection<HSSFCell> failed = statii.get("FAILED");
+
+		if (!failed.isEmpty()) {
+			List<HSSFRow> rows = Lists.newArrayListWithExpectedSize(failed.size());
+			for (HSSFCell cell : failed) {
+				HSSFRow row = cell.getRow();
+				rows.add(row);
+			}
+
+			Set<HSSFCell> compromisedThemeCells = Sets.newHashSet();
+
+			Map<String, Collection<HSSFCell>> themeMap = themes.asMap();
+			for (Map.Entry<String, Collection<HSSFCell>> mapEntry : themeMap.entrySet()) {
+				Collection<HSSFCell> themeCells = mapEntry.getValue();
+
+				boolean compromised = false;
+				for (Iterator<HSSFCell> iterator = themeCells.iterator(); !compromised && iterator.hasNext();) {
+					HSSFCell themeCell = iterator.next();
+					HSSFRow row = themeCell.getRow();
+					compromised = rows.contains(row);
+				}
+
+				if (compromised) {
+					compromisedThemeCells.addAll(themeCells);
+				}
+			}
+
+			Set<String> compromisedThemes = Sets.newHashSet();
+			for (HSSFCell themeCell : compromisedThemeCells) {
+				String contents = themeCell.getStringCellValue();
+				if (null != contents) {
+					Iterable<String> themes = Splitter.onPattern("\\s+").omitEmptyStrings().split(contents);
+					Iterables.addAll(compromisedThemes, themes);
+				}
+			}
+
+			for (String theme : compromisedThemes) {
+				Collection<HSSFCell> cells = themes.get(theme);
+				for (HSSFCell cell : cells) {
+					HSSFCellStyle cellStyle = cell.getCellStyle();
+					HSSFSheet sheet = cell.getSheet();
+					HSSFWorkbook workbook = sheet.getWorkbook();
+
+					HSSFFont originalFont = cellStyle.getFont(workbook);
+
+					HSSFCellStyle clone = workbook.createCellStyle();
+					clone.cloneStyleFrom(cellStyle);
+
+					HSSFFont font = workbook.findFont(
+						true,
+						IndexedColors.DARK_RED.getIndex(),
+						originalFont.getFontHeight(),
+						originalFont.getFontName(),
+						originalFont.getItalic(),
+						originalFont.getStrikeout(),
+						originalFont.getTypeOffset(),
+						originalFont.getUnderline());
+
+					if (null == font) {
+						font =  workbook.createFont();
+						font.setBold(true);
+						font.setColor(IndexedColors.DARK_RED.getIndex());
+						font.setFontHeight(originalFont.getFontHeight());
+						font.setFontName(originalFont.getFontName());
+						font.setItalic(originalFont.getItalic());
+						font.setStrikeout(originalFont.getStrikeout());
+						font.setTypeOffset(originalFont.getTypeOffset());
+						font.setUnderline(originalFont.getUnderline());
+					}
+					clone.setFont(font);
+					cell.setCellStyle(clone);
+				}
+			}
+		}
+	}
+
 
 	@Override
 	public void addSuite(JsonObject suite) {
